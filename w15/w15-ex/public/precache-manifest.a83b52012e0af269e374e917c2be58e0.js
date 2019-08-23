49fc4506b94baa909c300c81c40c4c1fd48f1ccc
prem <assaultprem@rediffmail.com>
2019-08-23 13:07:08 +0530

    static redux

---
+++ w15/w15-ex/public/precache-manifest.a83b52012e0af269e374e917c2be58e0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de58e6bc16e4e647cc1ffa84b462f110",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b49d4c728f13f1a947b5",
+    "url": "/static/js/2.c0f97a1a.chunk.js"
+  },
+  {
+    "revision": "13ce0312d32f0a45ecfd",
+    "url": "/static/js/main.82ba79b7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
